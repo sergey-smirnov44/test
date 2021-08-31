b79fc7a86c40e5b00ca8f47c3e1e3b07084ce769
Serega <agentserj@mail.ru>
2021-08-31 10:23:52 +0300

    push message

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 import * as data from './data.js'
 
 let allMessages = data.messages; // массив объектов из data.js передаем в allmessades
+let allUsers = data.users;
 let countFriends;// подсчет кол-ва друзей в левом меню
 let listChanels;// подсчет кол-ва каналов в левом меню
 
@@ -17,7 +18,6 @@ let listChanels;// подсчет кол-ва каналов в левом ме�
     countFriends = countFriends.length;
     document.getElementById('countFriends').innerHTML = countFriends;
 }
-
 
 function generate(messages) {
     let partMessage = '';
@@ -39,18 +39,7 @@ function generate(messages) {
     document.getElementById('example').innerHTML = partMessage;
 }
 
-
 generate(allMessages);
-
-// onclick(event) {
-//     let filtered = allMessages.filter(i => i.name === event);
-//     generate(filtered);
-// }
-
-// let allMessagesString = JSON.stringify(allMessages)
-// let filtered = allMessagesString.filter(messenge => messenge.length >  6)
-
-// console.log(allMessagesString)
 
 
 (function () {
@@ -58,52 +47,145 @@ generate(allMessages);
     let searchField = ''; // переменная для ошибки (пользователь ничего не ввел)
     let searchValue = document.getElementById('search');
     searchValue.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            if (searchValue.value == '') {
-                return;
-            }
 
-            // const userId = allMessages.filter((e) => {
-            //     return e.text == searchValue.value
-            // })
-            //
-            // console.log(userId)
 
-            /* ***************************************************************  поиск по id
-                 let user = allMessages.find(item => item.id == this.value)
-                 console.log(user);
-            */
-            // console.log(filtered)
-                alert(searchValue.value.toString())
-
-            this.value = '';
+        if (event.key !== 'Enter' || searchValue.value == '') {
+            return false;
         }
+
+        /*            *************Ужасный вариант поиска***************           */
+        let filtered = allMessages.filter(i => i.text === searchValue.value);
+        console.log(filtered.text)
+
+
+        generate(filtered);
+
+
+        this.value = '';
 
     });
 })();
 
-// const cities = [
-//     {name: 'Moscow', population: 12506468},
-//     {name: 'Saint Petersburg', population: 5351935},
-//     {name: 'Novosibirsk', population: 1612833},
-//     {name: 'Kaliningrad', population: 482443},
-//     {name: 'Kaluga', population: 336726}
-// ];
+for (let message in allMessages) {
+    let desiredMessage = Object.values(allMessages[message])
+    let arrMes = allMessages[message].text.split()
+    // console.log(desiredMessage)
+    const filtered = (desiredText) => {
+        return arrMes.filter(data => {
+            return data.toLowerCase().indexOf(desiredText.toLowerCase()) > -1;
+        })
+        // console.log(filtered("Which country to visit next? This is a photo with my friends - celebrating in Bali😎"))
 
-// function SearchText() {
-//     for (let message in allMessages) {
-//         let desiredMessage = allMessages[message]
-//         let obj = Object.values(desiredMessage);
-//         // console.log(desiredMessage)
-//         const filtered = (desiredText) => {
-//             return desiredMessage.filter(data => {
-//                 return data.toLowerCase().indexOf(desiredText.toLowerCase()) > -1;
-//             })
-//             console.log(filtered('e'))
-//
-//         }
-//     }
-// }
+
+    }
+    const filt = filtered("Wo");
+
+    // console.log(desiredMessage)
+    // console.log(arrMes )
+}
+;
+
+
+(function pushMessage() {
+    let addMessage = document.getElementById('addMessage');
+    addMessage.addEventListener('keydown', function (event) {
+        if (event.key !== 'Enter') {
+            return;
+        }
+        if (addMessage.value == '') {
+            return;
+        }
+
+        let theTime = new Date();
+
+        const newMessage = {
+            id: allMessages[allMessages.length - 1].id + 1,
+            name: "Sergey",
+            time: theTime.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true}),
+            date: "Today",
+            text: addMessage.value,
+            photo: "https://www.meme-arsenal.com/memes/ed0607a33edc9e0f8fe179c05fa0cf7f.jpg"
+        }
+
+        allMessages.push(newMessage)
+        generate(allMessages);
+        console.log(allMessages[5])
+
+        this.value = '';
+    });
+}());
+
+let listFriends = document.getElementsByClassName(`people`);
+
+
+
+/**************************Просмотр людских профилей************************/
+let newUser = [];
+let infouser = document.getElementById('infouser');
+let allFriends = document.getElementsByClassName('people');
+for (let i=1; i < allFriends.length; i++) {
+    allFriends[i].addEventListener('click', function (event) {
+
+        // for (let i=1; i<allFriends.length; i++) {
+        for (let key in allUsers) {
+            // infouser.style.display='block';
+            newUser += '<div class="infouser">'
+            newUser += `<img alt="${allUsers[key].photo}" class="infouser__photo" src="${allUsers[key].photo}">`
+            newUser += '<div class="info-column">'
+            newUser += '<p class="info-column__name">' + allUsers[key].name + '</p>'
+            newUser += '<p class="info-column__profession">' + allUsers[key].profession + '</p>'
+            newUser += '<ul class="social d-flex">'
+            newUser += '<li class="social__item social__item--fb"></li>'
+            newUser += '<li class="social__item social__item--tw"></li>'
+            newUser += '<li class="social__item social__item--ins"></li>'
+            newUser += '<li class="social__item social__item--in"></li>'
+            newUser += '</ul>'
+            newUser += '<div class="message d-flex align-items-center">'
+            newUser += '<button class="message__btn">Message</button>'
+            newUser += '<button class="message__more"></button>'
+            newUser += '</div>'
+            newUser += '<div class="username">'
+            newUser += '<p class="username__description">Username</p>'
+            newUser += '<p class="username__title">' + allUsers[key].username + '</p>'
+            newUser += '</div>'
+            newUser += '<div class="email">'
+            newUser += '<p class="email__description">Email</p>'
+            newUser += '<p class="email__title">' + allUsers[key].email + '</p>'
+            newUser += '</div>'
+            newUser += '<div class="skype">'
+            newUser += '<p class="skype__description">Skype</p>'
+            newUser += '<p class="skype__title">' + allUsers[key].skype + '</p>'
+            newUser += '</div>'
+            newUser += '<div class="timezone">'
+            newUser += '<p class="timezone__description">Timezone</p>'
+            newUser += '<p class="timezone__time">' + allUsers[key].timezone + '</p>'
+            newUser += '</div>'
+            newUser += '</div>'
+            newUser += '</div>'
+
+
+            //     allUsers[key].flag = true;
+            // if (allUsers[key].flag) {
+            //     document.getElementById('infouser').innerHTML = newUser;
+            // } else {
+            //     return false;
+            // }
+
+            listFriends[i].id == allUsers[key].id;
+
+            allUsers[i].flag = true ?  document.getElementById('infouser').innerHTML = newUser : false;
+
+        }
+
+        // if (allUsers[i].flag) {
+        //     document.getElementById('infouser').innerHTML = newUser;
+        // }
+
+    });
+    console.log( listFriends[5])
+
+}
+// console.log(infouser)
 
 
 
--- app/js/data.js
+++ app/js/data.js
@@ -2,6 +2,7 @@
     {
         "id": 1,
         "name": "Jeshua Stout",
+        "age": 18,
         "time": "6:38 PM",
         "date": "Monday, October 22nd",
         "text": "Which country to visit next? This is a photo with my friends - celebrating in Bali\uD83D\uDE0E",
@@ -10,7 +11,7 @@
     {
         "id": 2,
         "name": "Marcus Scott",
-        "time": "6:38 PM",
+        "time": "1:22 PM",
         "date": "Today",
         "text": "Wow!!!!",
         "photo": "https://cs13.pikabu.ru/avatars/2913/x2913029-1321135724.png"
@@ -18,7 +19,7 @@
     {
         "id": 3,
         "name": "Cherry cat",
-        "time": "6:38 PM",
+        "time": "8:13 PM",
         "date": "Monday, October 22nd",
         "text": "Which country to visit next?",
         "photo": "https://img.icons8.com/emoji/256/man-facepalming.png"
@@ -26,4 +27,126 @@
 
 ]
 
- export {messages}
+ const users = [
+     {
+         "id" : 1,
+         "name" : "Orlando Diggs",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "email": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "https://pressa.tv/uploads/posts/2016-01/1452843631_ya1.jpeg",
+         "flag": false
+     },
+     {
+         "id" : 2,
+         "name" : "Carmen Velasco",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "e-mail": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "https://trinixy.ru/pics5/20170524/beautiful_girls_01.jpg",
+         "flag": false
+     },
+     {
+         "id" : 3,
+         "name" : "Orlando Diggs",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "e-mail": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "https://tipik.ru/wp-content/uploads/2019/05/%D0%9A%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D0%B5-%D1%84%D0%BE%D1%82%D0%BE-16-%D0%BB%D0%B5%D1%82%D0%BD%D0%B5%D0%B3%D0%BE-%D0%BF%D0%B0%D1%80%D0%BD%D1%8F-%D0%B2%D0%BA-021.jpg",
+         "flag": false
+     },
+     {
+         "id" : 4,
+         "name" : "Orlando Diggs",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "e-mail": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "https://articles.checkperson.ru/storage/document_articles/2019/08-06/VluFk6LC/%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%B8%D1%82%D1%8C%20%D0%BF%D0%B0%D1%80%D0%BD%D1%8F%20%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD.jpg",
+         "flag": false
+     },
+     {
+         "id" : 5,
+         "name" : "Orlando Diggs",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "e-mail": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "http://www.lipetskmedia.ru/images/news_/112/398_big2.jpg",
+         "flag": false
+     },
+     {
+         "id" : 6,
+         "name" : "Orlando Diggs",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "e-mail": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "https://cdn.fishki.net/upload/post/2020/07/24/3377606/1551595409-2.jpg",
+         "flag": false
+     },
+     {
+         "id" : 7,
+         "name" : "Orlando Diggs",
+         "profession": "Programmer",
+         "social" : {
+             "facebook": "https://fb.com",
+             "twitter": "https://twitter.com",
+             "instagram": "https://instagram.com",
+             "linkedin": "https://linkedin.com"
+         },
+         "username": "ork_dig",
+         "e-mail": "diggs@rambler.com",
+         "skype": "",
+         "timezone" : "Your time",
+         "photo": "https://pressa.tv/uploads/posts/2016-01/1452843631_ya1.jpeg",
+         "flag": false
+     },
+ ]
+
+ export {messages, users}
